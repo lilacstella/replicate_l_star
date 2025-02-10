e5db61cbc85cef117e2b1217342521bc14a9aba1
Stella Yang <stella@stellayang.dev>
2025-02-10 00:26:42 -0600

    list comprehension the S • Sigma and initial populate table

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 def is_member(q):
     print(q)
-    a = input(f'Is {q} in the language?')
-    return 1 if a == 'y' else 0
+    a = input(f'Is "{q}" in the language?')
+    return True if a == 'y' else False
 
-def is_equivalent(self, grammar):
+def is_equivalent(grammar):
     print(grammar)
     a = input('Is the grammar correct?')
     return a == 'y', []
 
 def s_concat_alphabet(S, alphabet):
-    output = []
-    for s in S:
-        for a in alphabet:
-            if s + a not in S:
-                output.append(s + a)
-    return output
+    return [s + a for s in S for a in alphabet if s + a not in S]
 
 def main():
     # we have an observation table O
+    # it has a rows and columns
+    # going to represent each row as an array
+    # with indices corresponding to E
     O: dict = {}
     alphabet = list('ab')
     S: list = ['']
     E: list = ['']
-    # is it closed?
-    print(s_concat_alphabet(S, alphabet))
-    # for s in s_concat_alphabet(S, alphabet):
 
+    # populate table
+    O[''] = [is_member('')]
+    for a in alphabet:
+        O[a] = [is_member(a)]
+
+    print(s_concat_alphabet(S, alphabet))
+    # is it closed?
+    for i in s_concat_alphabet(S, alphabet):
+        O[i] = [is_member(i)]
     # is it consistent?
 
     # present to the teacher
