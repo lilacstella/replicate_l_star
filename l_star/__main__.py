0a6a187b759a85207a3340f29630a0ab08e291f5
Stella Yang <stella@stellayang.dev>
2025-02-11 01:18:54 -0600

    implement consistent function

--- l_star/__main__.py
+++ l_star/__main__.py
@@ -40,6 +40,28 @@ def closed(O, S):
             return False, i
     return True, None
 
+def consistent(O, S):
+    # for every type or row value in S section of O
+    # do they go to the same row type if the next character is
+    # every character in the alphabet?
+    # can these things be generators? <- TODO
+    for matching_row_s in find_duplicate_rows(O, S):
+        # do all s in matching_row_s go to the same place?
+        # len(matching_row_s) >= 2
+        # matching_row_s looks like ['a', 'b'], keys s in S that have the same row values
+        # as of rn, they are completely matching,
+        # we're going to check if that continues to be the case with concat + a in alphabet
+        x = matching_row_s[0]
+        for y in matching_row_s[1:]:
+            for a in alphabet:
+                if O[x + a] == O[y + a]:
+                    continue
+                # need to figure out what to add to E
+                for i, boolean_pair in enumerate(zip(O[x + a], O[y + a])):
+                    if boolean_pair[0] != boolean_pair[1]:
+                        return False, a + E[i]
+                raise ValueError(f"States {x} and {y} reached here with {a}")
+    return True, None
 
 if __name__ == '__main__':
     l_star()
