4e7e2928a5953a32d90a4fca9c94091126faf18e
Stella Yang <stella@stellayang.dev>
2025-03-07 10:30:13 -0600

    add some type specification on the return types in dfa.py

--- l_star/dfa.py
+++ l_star/dfa.py
@@ -1,24 +1,28 @@
 from l_star import alphabet
 from automathon import DFA as DFA_VISUALIZATION
+from graphviz import ExecutableNotFound
 
 class DeterministicFiniteAutomaton:
-    def __init__(self, starting_state):
+    def __init__(self, starting_state: str):
         self.states: set = set()
         self.transitions: dict[str: dict[str: str]] = {}
         self.start_state: str = starting_state
         self.accepting_states: set = set()
         self.regex = None
 
-    def add_state(self, state: str, accepting=False):
+    def add_state(self, state: str, accepting=False) -> None:
         self.states.add(state)
         if accepting:
             self.accepting_states.add(state)
 
-    def add_transition(self, src, symbol, dest):
+    def add_transition(self, src, symbol, dest) -> None:
         if src not in self.transitions:
             self.transitions[src] = {}
         self.transitions[src][symbol] = dest
 
-    def view(self):
+    def view(self) -> None:
         automata = DFA_VISUALIZATION(self.states, alphabet, self.transitions, self.start_state, self.accepting_states)
-        automata.view("DFA")
+        try:
+            automata.view("DFA")
+        except ExecutableNotFound:
+            print("Please install graphviz and add it to your PATH to visualize the DFA")
