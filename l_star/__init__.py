fe7948f419f532b25af7f7658217405d12a9b5d4
Stella Yang <stella@stellayang.dev>
2025-03-07 11:02:30 -0600

    Update __init__.py

--- l_star/__init__.py
+++ l_star/__init__.py
@@ -7,4 +7,9 @@ teacher = Teacher()
 S: list = ['']  # prefixes (rows)
 E: list = ['']  # postfixes (columns)
 
-from .__main__ import l_star
+def l_star():
+    from .__main__ import l_star as l_star_main
+    l_star_main()
+
+# Ensure that l_star is accessible at the package level
+__all__ = ['l_star']
